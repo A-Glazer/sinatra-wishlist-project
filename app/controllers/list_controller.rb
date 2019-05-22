84dca15cb55a3aaa548d6cb57449acbc26103078
imaglazer <avigayilglazer@gmail.com>
2019-05-22 23:27:41 +0300

    added list/edit

--- app/controllers/list_controller.rb
+++ app/controllers/list_controller.rb
@@ -2,15 +2,40 @@ class ListController < ApplicationController
 
     get '/view/:id' do
         @user = User.find_by_id(session[:user_id])
-        # binding.pry
             if is_logged_in?
+                @items = Item.all
             erb :"/list/view"
             else
                 erb :"users/error"
             end
-       
     end
 
+    get '/new/:id' do
+        @user = User.find_by_id(session[:user_id])
+            if is_logged_in?
+                @items = Item.all
+            erb :"/list/new"
+            else
+                erb :"users/error"
+            end
+    end
 
+    post '/list/view' do
+        @user = User.find_by_id(session[:user_id])
+        @items = Item.create(name: params[:name], quantity: params[:quantity])
+        # binding.pry
+        @user.items << @items
+        redirect 'view/:id'
+    end
+
+    get '/list/:id/edit' do
+        @user = User.find_by_id(session[:user_id])
+        if is_logged_in?
+            @items = Item.find_by(id: params[:id])
+        erb :"/list/edit"
+        else
+            erb :"users/error"
+        end
+    end
 end
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190521190031) do
+ActiveRecord::Schema.define(version: 20190522195342) do
 
   create_table "items", force: :cascade do |t|
     t.string  "name"
     t.integer "quantity"
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
