b3a1b11b73b856f32732f4d57b26fe8bcc246c61
imaglazer <avigayilglazer@gmail.com>
2019-05-23 18:55:06 +0300

    figuring out error in edit

--- app/controllers/list_controller.rb
+++ app/controllers/list_controller.rb
@@ -25,14 +25,28 @@ class ListController < ApplicationController
         @items = Item.create(name: params[:name], quantity: params[:quantity])
         # binding.pry
         @user.items << @items
-        redirect 'view/:id'
+        redirect to :"/view/:id"
     end
 
     get '/list/:id/edit' do
         @user = User.find_by_id(session[:user_id])
+        # binding.pry
         if is_logged_in?
+            binding.pry
             @items = Item.find_by(id: params[:id])
+
         erb :"/list/edit"
+        else
+            erb :"users/error"
+        end
+    end
+
+    patch '/list/:id' do
+        @user = User.find_by_id(session[:user_id])
+        if is_logged_in?
+            @items.update(params[:items])
+            # binding.pry
+            redirect to :"/view/:id"
         else
             erb :"users/error"
         end
